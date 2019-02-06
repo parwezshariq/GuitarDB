66a97234c965ac147cf7d9f0d7035bc738143333
shariqparwez <shariqparwez@outlook.com>
2019-02-06 16:06:26 +0530

    @Repository annotation added - not mandatory

--- src/main/java/com/shariqparwez/db/repository/ModelTypeJpaRepository.java
+++ src/main/java/com/shariqparwez/db/repository/ModelTypeJpaRepository.java
@@ -1,9 +1,11 @@
 package com.shariqparwez.db.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.shariqparwez.db.model.ModelType;
 
+@Repository
 public interface ModelTypeJpaRepository extends JpaRepository<ModelType, Long> {
 
 }
