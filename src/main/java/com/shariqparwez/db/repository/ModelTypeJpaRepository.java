5035d284b4001f7fd659a995f7a7c216044aaa4e
shariqparwez <shariqparwez@outlook.com>
2019-02-06 15:25:06 +0530

    ModelTypeRepository to JpaRepository

---
+++ src/main/java/com/shariqparwez/db/repository/ModelTypeJpaRepository.java
@@ -0,0 +1,9 @@
+package com.shariqparwez.db.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shariqparwez.db.model.ModelType;
+
+public interface ModelTypeJpaRepository extends JpaRepository<ModelType, Long> {
+
+}
