b4f6b72329aa1a5982994b77abbe5857c5f3ace3
shariqparwez <shariqparwez@outlook.com>
2019-02-07 13:16:32 +0530

    QueryDSL - findByStateLike

--- src/main/java/com/shariqparwez/db/repository/LocationJpaRepository.java
+++ src/main/java/com/shariqparwez/db/repository/LocationJpaRepository.java
@@ -1,5 +1,7 @@
 package com.shariqparwez.db.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.shariqparwez.db.model.Location;
 @Repository
 public interface LocationJpaRepository extends JpaRepository<Location, Long> {
 
+	List<Location> findByStateLike(String stateName);
+	
 }
