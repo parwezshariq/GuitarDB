cc8ad97b6d2b2535a9544f5479583fc0fddc7ce6
shariqparwez <shariqparwez@outlook.com>
2019-02-09 12:12:12 +0530

    NamedNativeQueries Usage

--- src/main/java/com/shariqparwez/db/repository/ManufacturerJpaRepository.java
+++ src/main/java/com/shariqparwez/db/repository/ManufacturerJpaRepository.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.shariqparwez.db.model.Manufacturer;
@@ -17,5 +18,6 @@ public interface ManufacturerJpaRepository extends JpaRepository<Manufacturer, L
 	
 	List<Manufacturer> findByActiveFalse();
 	
+	List<Manufacturer> getAllThatSellAcoustics(String name);
 	
 }
